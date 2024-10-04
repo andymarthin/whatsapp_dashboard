28ce75ca4a497e1d06453048fcf2ebf707b09516
Andy Marthin <mr.andymarthin@gmail.com>
2024-10-04 15:45:29 +0700

    Fix: codelint

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -5,7 +5,7 @@ require "test_helper"
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium,
     using: (ENV["SHOW_BROWSER"] ? :chrome : :headless_chrome),
-    screen_size: [1400, 1400] do |options|
+    screen_size: [ 1400, 1400 ] do |options|
       # Allows running in Docker
       options.add_argument("--disable-dev-shm-usage")
       options.add_argument("--no-sandbox")
--- test/support/capybara.rb
+++ test/support/capybara.rb
@@ -3,12 +3,12 @@
 ActiveSupport.on_load(:action_dispatch_system_test_case) do
   require "capybara"
   require "capybara/rails"
-  
+
   Capybara.configure do |config|
     config.default_max_wait_time = 2
     config.save_path = "tmp/screenshots"
     config.enable_aria_label = true
-    config.server = :puma, {Silent: true}
+    config.server = :puma, { Silent: true }
     config.test_id = "data-testid"
   end
 end
