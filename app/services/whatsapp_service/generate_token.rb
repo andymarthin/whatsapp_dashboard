f8e94e4618431aba8e759dfcb3d1518909aa63e3
Andy Marthin <mr.andymarthin@gmail.com>
2024-10-13 20:17:45 +0700

    Change: generate token use META_APP_SECRET

--- app/services/whatsapp_service/generate_token.rb
+++ app/services/whatsapp_service/generate_token.rb
@@ -1,12 +1,9 @@
 module WhatsappService
   class GenerateToken < ApplicationService
     def call
-      hmac_secret = Rails.application.credentials.jwt_secret_key
-      expired = 5.years.from_now
+      hmac_secret = ENV["META_APP_SECRET"]
       iat ||= Time.zone.now.to_i
       payload = {
-        host: ENV["HOST"],
-        exp: expired.to_i,
         iat: iat
       }
       JWT.encode payload, hmac_secret, "HS256"
