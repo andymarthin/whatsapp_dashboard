deb357b77ccbfdd6cf8747ae2651adda9b01d7d5
Andy Marthin <mr.andymarthin@gmail.com>
2024-10-13 20:21:58 +0700

    Remove: checking host verify token

--- app/services/whatsapp_service/verify.rb
+++ app/services/whatsapp_service/verify.rb
@@ -6,13 +6,12 @@ module WhatsappService
 
     def call
       hmac_secret = Rails.application.credentials.jwt_secret_key
-      payload = JWT.decode(
+      JWT.decode(
         token,
         hmac_secret,
         true
       )
-      host = payload.dig(0, "host")
-      host.eql?(ENV["HOST"])
+
     rescue JWT::ExpiredSignature, JWT::VerificationError, JWT::DecodeError => e
       raise Authentication::Error.new e.message
     end
