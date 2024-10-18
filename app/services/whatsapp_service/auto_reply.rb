a6243a53d18b95fe74c6a73bba9638eadacbcd48
Andy Marthin <mr.andymarthin@gmail.com>
2024-10-19 05:11:06 +0700

    Change: improve auto reply code

--- app/services/whatsapp_service/auto_reply.rb
+++ app/services/whatsapp_service/auto_reply.rb
@@ -6,22 +6,41 @@ module WhatsappService
     end
 
     def call
-      if message_type.eql?("text") && room.bot?
-         data = Kredis.hash(room.id)
-         if data.to_h.present?
-          service_handle(data)
-         else
-          initialize_bot
-         end
+      return unless valid_message_type?
+
+      if text_message_for_bot?
+        handle_text_message
+      elsif interactive_message?
+        handle_interactive_message
       end
-      interactive_bot if message_type.eql?("interactive")
     end
 
     private
 
-    def initialize_bot
-      question = Question.first
+    def valid_message_type?
+      %w[text interactive].include?(message_type)
+    end
+
+    def text_message_for_bot?
+      message_type == "text" && room.bot?
+    end
+
+    def interactive_message?
+      message_type == "interactive"
+    end
+
+    def handle_text_message
+      data = Kredis.hash(room.id)
+      data.to_h.present? ? service_handle(data) : initialize_bot
+    end
+
+    def handle_interactive_message
+      question = Question.find_by(id: interactive_id)
       interactive_bot(question)
+    end
+
+    def initialize_bot
+      interactive_bot(Question.first)
     end
 
     def send_list(question, message = nil)
@@ -33,40 +52,31 @@ module WhatsappService
       Send::Interactive.call(phone_number, interactive)
     end
 
-    def interactive_bot(question = nil)
-      question ||= Question.find_by(id: interactive_id)
+    def interactive_bot(question)
       return initialize_bot if question&.main_menu?
-
       return unless question
 
       case question.question_type
-      when "location"
-        latitude = question.dig("options", "latitude")
-        longitude = question.dig("options", "longitude")
-        Send::Location.call(phone_number, latitude, longitude)
-      when "cs"
-        message = question.body
-        room.update(bot: false, open_until: 24.hours.from_now)
-        Send::Text.call(to: phone_number, text: message)
-      when "list"
-        send_list(question)
-      when "list_buttons"
-        interactive = Interactive::Button.call(question)
-        Send::Interactive.call(phone_number, interactive)
-      when "previous_menu"
-        parent = question.parent
-        interactive_bot(parent)
-      when "image"
-        caption = question.body
-        media_id = question.media_id
-        Send::Media.call(phone_number, media_id, "image", caption:)
+      when "location"    then send_location(question)
+      when "cs"          then handle_customer_service(question)
+      when "list"        then send_list(question)
+      when "list_buttons" then send_button_list(question)
+      when "previous_menu" then handle_previous_menu(question)
+      when "image"       then send_image(question)
       else
-        if question["type"].eql?("service")
-          Kredis.hash(room.id).update(id: question["id"])
-        end
-        answer = question.body
-        Send::Text.call(to: phone_number, text: answer) if answer.present?
+        handle_default_question(question)
       end
+    end
+
+    def send_location(question)
+      latitude = question.dig("options", "latitude")
+      longitude = question.dig("options", "longitude")
+      Send::Location.call(phone_number, latitude, longitude)
+    end
+
+    def handle_customer_service(question)
+      room.update(bot: false, open_until: 24.hours.from_now)
+      send_text(question.body)
     end
 
     def build_sections(sections, title: "Options")
@@ -96,18 +106,45 @@ module WhatsappService
        params.dig("messages", 0, "interactive", interactive_type, "id")
     end
 
+    def update_kredis(question)
+      Kredis.hash(room.id).update(id: question["id"])
+    end
+
+    def send_text(text)
+      Send::Text.call(to: phone_number, text: text)
+    end
+
+    def send_button_list(question)
+      interactive = Interactive::Button.call(question)
+      Send::Interactive.call(phone_number, interactive)
+    end
+
+    def handle_previous_menu(question)
+      interactive_bot(question.parent)
+    end
+
+    def send_image(question)
+      Send::Media.call(phone_number, question.media_id, "image", caption: question.body)
+    end
+
+    def handle_default_question(question)
+      update_kredis(question) if question["type"] == "service"
+      send_text(question.body) if question.body.present?
+    end
+
     def service_handle(data)
       current_session = data.to_h
       question = Question.find_by(id: current_session[:id])
       return unless question
 
       service_answer = question["service_answer"]&.gsub("#INPUT", raw_message)
-      list = question["sections"]
-      if list.present?
+
+      if question["sections"].present?
         send_list(question)
-      else
-        Send::Text.call(to: phone_number, text: service_answer) if service_answer.present?
+      elsif service_answer.present?
+        send_text(service_answer)
       end
+
       data.del
     end
   end
